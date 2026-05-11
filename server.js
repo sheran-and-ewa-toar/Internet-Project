47b32f6bf6a6e1603fc1264bcf77d1e69db85239
ewa <u9093082@bsmch.net>
2026-05-11 08:44:09 +0300

    add new resource types

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 const usersRoutes = require('./routes/usersRoutes');
 const modelsRoutes = require('./routes/modelsRoutes');
+const modelTypesRoutes = require('./routes/modelTypesRoutes');
+const featureSetsRoutes = require('./routes/featureSetsRoutes');
+const featureFiltersRoutes = require('./routes/featureFiltersRoutes');
 
 const loggerMiddleware = require('./middleware/loggerMiddleware');
 
@@ -13,6 +16,9 @@ app.use(loggerMiddleware);
 
 app.use('/users', usersRoutes);
 app.use('/models', modelsRoutes);
+app.use('/model-types', modelTypesRoutes);
+app.use('/feature-sets', featureSetsRoutes);
+app.use('/feature-filters', featureFiltersRoutes);
 
 const PORT = 3000;
 
